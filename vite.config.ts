1a22d4b7251618aa347a2ad4d93f114334c93bc6
Tripcoders <92749647+Tripcoders@users.noreply.github.com>
2025-04-07 16:39:03 +0200

    Build full application to docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/travelsecrets/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
